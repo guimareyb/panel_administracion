53feea122b978d96c00665632dd4e28d29b8884b
carolina <caroibarra2110@gmail.com>
2023-12-01 22:10:53 -0500

    Se realiza endpoint de permission

--- src/main/java/com/panel_administracion/controller/PermissionController.java
+++ src/main/java/com/panel_administracion/controller/PermissionController.java
@@ -2,13 +2,15 @@ package com.panel_administracion.controller;
 
 import com.panel_administracion.domain.content.ContentRepository;
 import com.panel_administracion.domain.permission.*;
+import com.panel_administracion.domain.roles.DataRolesResponse;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -33,5 +35,31 @@ public class PermissionController {
         return ResponseEntity.created(url).body(dataPermissionResponse);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DataPermissionResponse> responseDataPermission(@PathVariable Long id){
+        Permission permission = permissionRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DataPermissionResponse(permission));
+    }
 
+    @GetMapping
+    public ResponseEntity<Page<DataPermissionResponse>> listPermission(@PageableDefault(size = 3, sort = "id")Pageable pageable){
+        return ResponseEntity.ok(permissionRepository.findByFlagTrue(pageable).map(DataPermissionResponse::new));
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<DataPermissionResponse> updataPermission (@RequestBody @Valid DataPermissionUpdate dataPermissionUpdate){
+        Permission permission = permissionRepository.getReferenceById(dataPermissionUpdate.id());
+        permission.updatePermission(dataPermissionUpdate);
+        DataPermissionResponse dataPermissionResponse = new DataPermissionResponse(permission);
+        return ResponseEntity.ok(dataPermissionResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> deletePermission(@PathVariable Long id){
+        Permission permission = permissionRepository.getReferenceById(id);
+        permission.deactivatePermission();
+        return ResponseEntity.noContent().build();
+    }
 }
